1826369d6a72036acd6239e36474bcd0a0dbddbd
Miguel Leonar <migsldev@gmail.com>
2024-02-04 15:19:58 +1100

    updated functions in index.js

--- index.js
+++ index.js
@@ -1,95 +1,145 @@
 const container = document.querySelector('.container');
-const search = document.querySelector('.search-box button');
-const weatherBox = document.querySelector('.weather-box');
-const weatherDetails = document.querySelector('.weather-details');
-const error404 = document.querySelector('.not-found');
-
-const image = document.querySelector('.weather-box img');
-const temperature = document.querySelector('.weather-box .temperature');
-const description = document.querySelector('.weather-box .description');
-const humidity = document.querySelector('.weather-details .humidity span');
-const wind = document.querySelector('.weather-details .wind span');
-
 
 const toggleModeBtn = document.getElementById('toggleMode');
 const body = document.body;
 
-search.addEventListener('click', () => {
+function getWeather() {
+
 
     const APIKey = '5c7a95fa6e3ec618a37a2f508957c4bb';
-    const city = document.querySelector('.search-box input').value;
+    const city = document.getElementById('city').value;
 
-    if (city === '')
+    if (!city){
+        //error message if search box empty
+        alert('Please enter a city');
         return;
+    }
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&units=metric&appid=${APIKey}`)
+    //API openweathermap - current weather and hourly forecast
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&
+    units=metric&appid=${APIKey}`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?units=metric&q=${city}&units
+    =metric&appid=${APIKey}`;
+
+    // Current Weather
+    fetch(currentWeatherUrl)
         .then(response => response.json())
-        .then(json => {
+        .then(data => 
+            { displayWeather(data);
+            })
+            .catch(error => {
+                console.error('Error fetching current weather data:', error);
+                alert('Error fetching current weather data. Please try again.');
+            });
+    
+    // Hourly weather
+    fetch(forecastUrl)
+        .then(response => response.json())
+        .then(data => 
+            { displayHourlyForecast(data.list);
+            })
+            .catch(error => {
+                console.error('Error fetching current weather data:', error);
+                alert('Error fetching current weather data. Please try again.');
+            });
 
-            if (json.cod === '404') {
-                container.style.height = '400px';
-                weatherBox.style.display = 'none';
-                weatherDetails.style.display = 'none';
-                error404.style.display = 'block';
-                error404.classList.add('fadeIn');
-                return;
-            }
+    
+}
 
-            error404.style.display = 'none';
-            error404.classList.remove('fadeIn');
+//display the weather data based from the API
+    // take weather data, extracts weather information, updates the HTML elements, 
+    //displays error message if there is an issue with API request
 
-            let cloudPresent = json.weather.some(weatherItem => weatherItem.main.toLowerCase().includes('cloud'));
+//display current weather
+function displayWeather(data) {
+    const tempDivInfo = document.getElementById('temp-div');
+    const weatherInfoDiv = document.getElementById('weather-info');
+    const weatherIcon = document.getElementById('weather-icon');
+    const hourlyForecastDiv = document.getElementById('hourly-forecast');
 
-            if(cloudPresent) {
-                image.src = 'images/cloud.png';
-            } else {
-            
-                json.weather.forEach(weatherItem => {
-                    switch (json.weather[0].main) {
-                        case 'Clear':
-                            image.src = 'images/clear.png';
-                            break;
+    //Clearing previous content after user input
+    weatherInfoDiv.innerHTML = "";
+    hourlyForecastDiv.innerHTML = "";
+    tempDivInfo.innerHTML = "";
 
-                        case 'Rain':
-                            image.src = 'images/rain.png';
-                            break;
+    if (data.cod === '404') {
+        weatherInfoDiv.innerHTML = `<p>${data.message}</p>`
+    } else {
+        const cityName = data.name;
+        const temperature = parseInt(data.main.temp); // converts to Celsius
+        const description = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
 
-                        case 'Snow':
-                            image.src = 'images/snow.png';
-                            break;
+        const temperatureHTML = `<p>${temperature}°C</p>`;
+        const weatherHTML = `
+            <p>${cityName}</p>
+            <p>${description}</p>`;
 
-                        case 'Haze':
-                            image.src = 'images/mist.png';
-                            break;
-
-                        default:
-                            image.src = '';
-                    }
-                });
-            }
-
-            temperature.innerHTML = `${parseInt(json.main.temp)}°C`;
-            description.innerHTML = `${json.weather[0].description}`;
-            humidity.innerHTML = `${json.main.humidity}%`;
-            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
-
-            weatherBox.style.display = '';
-            weatherDetails.style.display = '';
-            weatherBox.classList.add('fadeIn');
-            weatherDetails.classList.add('fadeIn');
-            container.style.height = '590px';
+        tempDivInfo.innerHTML = temperatureHTML;
+        weatherInfoDiv.innerHTML = weatherHTML;
+        weatherIcon.src = iconUrl
+        weatherIcon.alt = description;
 
 
-        });
+        //shows the weather icon visible
+            // will occur 
+        showImage();
+
+    }
+}
+
+//display hourly forecast
+    //takes the hourly weather
+    //extracts the relevant information of 24 hours
+    //creates element each hour
+    //updated in the hourlyForecastDiv
+function displayHourlyForecast(hourlyData) {
+
+    const hourlyForecastDiv = document.getElementById('hourly-forecast');
+    const next24Hours = hourlyData.slice(0, 8); // displaying the hourly forecast in 3 hour intervals -> slice the 24 hour data, extracts the first 8 items 
+
+    //iterate over each hourly data and create content to display 
+        //using a forEach loop to iterate in the next25Hours array
+            // for each item it extracts the time, temp in celsius, weather icon code 
+    next24Hours.forEach(item => {
+        const dateTime = new Date(item.dt * 1000);
+        const hour = dateTime.getHours();
+        const temperature = parseInt(item.main.temp);
+        const iconCode = item.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`
+
+        //creates a string = string for each hourlyItemHtml icon
+        const hourlyItemHtml = `
+        <div class = "hourly-item">
+            <span>${hour}:00</span>
+            <img src="${iconUrl}" alt="Hourly Weather Icon">
+            <span>${temperature}°C</span>
+        </div>`;
+
+        //append the created hourlyItemHtml to the hourlyForecastDiv
+        hourlyForecastDiv.innerHTML += hourlyItemHtml;
 
 
+    });
+}
+
+//displaying the weather icon
+function showImage() {
+    const weatherIcon = document.getElementById('weather-icon');
+    weatherIcon.style.display = 'block'; //changing the css style for the weatherIcon div in html
+}
+
+// Event listener for click in the search input
+document.querySelector('.search-box input').addEventListener('click', (event) => {
+    search.click();
 });
 
 // Event listener for Enter key press in the search input
 
 document.querySelector('.search-box input').addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
-        search.click();
+        getWeather();
     }
 });
 
